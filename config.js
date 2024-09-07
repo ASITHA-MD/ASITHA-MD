acde30418683d5e6e571ce2412db5e3c74b57478
Eranda <ekasun473@gmail.com>
2024-09-07 14:28:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "iItkhBwS#cO4lTDkx06a0lCurC38tS1gMIBDASowDf3ZtABpHbwY",
+MONGODB: process.env.MONGODB || "94742223168",//enter mongo db url
 };
