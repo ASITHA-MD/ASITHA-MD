8c3548c43d11ecb1e14cfb63542e419cc74aac63
Dagaya <lakshansupun877@gmail.com>
2024-09-24 14:58:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "EjNSSKxI#Yq0ACTBDz8KM2iK-Ub4f_bqbedSARLTVakww7h-1iTI",
+SESSION_ID: process.env.SESSION_ID || "AyUUCZBY#Gv4ytu7ZK5UTvg__FBoQwpL8bpSjj-RkNbUiEJ7gxEk",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
