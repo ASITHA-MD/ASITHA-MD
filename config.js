fff7a96eecf629180edb4a4cc7c5800c07319be4
dasunmaduka <125837525+dasunmaduka@users.noreply.github.com>
2024-09-26 04:54:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "5i9AhT5I#DwXEt6KNVQJRubMrO9icQTP-APq72hpusCl1Rjvr970",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
