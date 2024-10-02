e1c035e9a41d17e94cd8bea81be1aba9073d7d2c
thatone117 <mosolosebetha2@gmail.com>
2024-10-02 21:05:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=TVFDGbzI#hayMD1idjKIXMfF-sW7q9i_zxwsnifpYfpnXqBgh0yk",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
