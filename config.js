4c14567413ecd50fcf94819aeed1dc87e132bde0
kushanmadusanka <114823733+kushanmadusanka@users.noreply.github.com>
2024-10-04 16:25:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=CZs2GAja#2j7q6GMZj5-AvBOvnkHoxq9X8ArsGFwMjw5jkdT6WVQ",
+MONGODB: process.env.MONGODB || "mongodb+srv://Kushan:1234@cluster0.kgci8.mongodb.net/",//enter mongo db url
 };
