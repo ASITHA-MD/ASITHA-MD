dc69eb22ab0c5ea3d6e5952422ae859178cdc607
Kawiya856 <kawiyadissanayaka@gmail.com>
2024-10-05 08:18:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || 𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=GYEy1QqZ#hecSj4I8DFx8eYUXXeSi56eygJPbKQMMz19yuMNfgcc",
+MONGODB: process.env.MONGODB || "94772484733",//enter mongo db url
 };
