06469c4d72bfa260e91510860aa53827cb9171f8
Demonlord1000000 <165568365+Demonlord1000000@users.noreply.github.com>
2024-10-07 21:56:14 +0530

    DLconfig.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=erpQSART#EoST3Vaxlv1Bts1mqsCdt9HQUMNZ-urR13p1QPFmVbo",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
