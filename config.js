f20ba2f2ac51fb79d039b95b2151713789060962
janurapahan <pahanjanura413@gmail.com>
2024-10-08 19:09:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=P2YyFKhD#EawfMrTd8aQkdugldR-JChes8gx6he9ko5PWIGNHrXM",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
