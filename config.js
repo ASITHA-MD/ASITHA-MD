05c8d181dc48d3f84dc665e0acebac19308b9397
SohailModz <162996452+SohailmodsXD@users.noreply.github.com>
2024-10-11 16:33:34 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=i6ACiDgA#ZFQMRHk7_LcocGWR89OAX32LAQOjklaGgBsES52JibI",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
