1d03ac1d7d316e8e7ff5e653b9955cff41800044
Noti-king-vajira-md <rashikasadaruwan07@gmail.com>
2024-10-15 09:51:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=XuISVLoa#tVgGLHwfNB8QfH4SPQEXWRpccqMA_s7Gu-xdWhgbCr0",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
