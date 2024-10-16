d2a36e43b988413a673b7a250a5d9ca01902300c
Anuhasking <mikiyacj18@gmail.com>
2024-10-16 08:11:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,34 @@
-𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=jmhiHT4A#10hLYFmajo-Xv4c3dOec1BwZSLU28vUMtJNKYJe63I0
+
+name: Node.js CI
+
+on:
+  push:
+    branches:
+      - main
+  pull_request:
+    branches:
+      - main
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        node-version: [20.x]
+
+    steps:
+    - name: Checkout repository
+      uses: actions/checkout@v3
+
+    - name: Set up Node.js
+      uses: actions/setup-node@v3
+      with:
+        node-version: ${{ matrix.node-version }}
+
+    - name: Install dependencies
+      run: npm install
+
+    - name: Start application
+      run: npm start
