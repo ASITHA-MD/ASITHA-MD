e87d479e914d7962701c24e20cca503e6b2560a6
Anuhasking <mikiyacj18@gmail.com>
2024-10-16 09:14:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=vrgklSaB#rs1S1x3Mq99NJHK3mrkS0p_ThdhhLrpdJ-hYBwoGbPU",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=jiQhSDiC#UPGfX3bLOQ4xvUZZge7bEOtPV8XBj9cKeT1dlGESR7g: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
