650ea468ae4bd007bef4d08984f5e595dc61b9c1
ls-arch <lspain784@gmail.com>
2024-10-19 18:05:07 +0100

    git commit -am 'initialize' git push -u origin main

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=zrRyXJhI#g9sd2qFDY3XODrELx7IPD3H-s7qsIm7sbaa9eAhOXY8",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
