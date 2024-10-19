9ff6e7930894749a5141940b0452318114d9ad7d
ls-arch <lspain784@gmail.com>
2024-10-19 15:51:20 +0100

    git commit -am 'config.js'

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=CvJRmRgQ#O371eqx5QeRChkRnMv_bOMfOA0h-7e6y8oeSIn8Btq0",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
