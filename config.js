f7c351502f7c6ad7c74abdf941769deefb80a604
athulakumara604 <143864889+athulakumara604@users.noreply.github.com>
2024-10-20 08:56:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
+MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
