cddc5b0d26f51bed5b9d20c7b34f3b2a63aefe0c
Ahmadqureshi01 <ahmadqureshi738@gmail.com>
2024-10-23 08:56:14 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=zqJ2SbwA#Anz_hsLq41KLJ9V9Ilp2cjjytjFfdn-vteqD91W3Rco",
+MONGODB: process.env.MONGODB || "https://wa.me/qr/L2UEFP26UQB4J1",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
