83a6bb571fdf0162c15fdcafc51bf592432b8b33
SandeepsAx <Sportwey0@gmail.com>
2024-10-23 12:43:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=ryQ3mZaR#MEpSevvBu0Ir__fMFaXMLM9PS9dG4UWdOGi5Ww8GSh8",
+MONGODB: process.env.MONGODB || "https://downloads.mongodb.com/compass/mongodb-compass-1.44.5-win32-x64.exe",
 };
